e13d311c8eb080f2e6dbcc2fea0155382f953d63
ced01 <cedlautard@gmail.fr>
2015-03-09 14:36:52 +0100

    Oublis de trois pages

---
+++ ConnectionInscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+           <meta charset="utf-8" />
+            <title>Connection/Inscription</title>
+            <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+    </head>
+     <body>
+        <div>
+
+
+        <nav>
+            <p>
+
+             
+            <ul>
+                
+                <li><a href="Accueil.php">Accueil</a></li>
+                <li><a href="Recherche.php">Voir les offres</a></li>
+                <li><a href="FAQ.php">A propos</a></li>
+                <li><a href="Forum.php">Forum</a></li>
+                <li><a href="ConnectionInscription.php">Connection/Inscription</a></li>
+
+                
+            </ul>
+            </p>
+        </nav>
+        </div>
+     
+     </body>
+    </html>
