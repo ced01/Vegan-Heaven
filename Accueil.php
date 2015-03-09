17170b33126ca7bebc9a8aafb4a9510e3bc89aa4
ced01 <cedlautard@gmail.fr>
2015-03-09 13:48:13 +0100

    Création de la structure HTML des pages d'accueil et de recherche, la page de recherche est encore vide

---
+++ Accueil.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+           <meta charset="utf-8" />
+            <title>VeganHeaven</title>
+            <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <![endif]-->
+    </head>
+     <body>
+        <div>
+
+
+        <nav>
+            <p>
+
+             
+            <ul>
+                
+                <li><a href="Accueil.php">Accueil</a></li>
+                <li><a href="Recherche.php">Voir les offres</a></li>
+                <li><a href="A propos.php">A propos</a></li>
+                <li><a href="Forum.php">A propos</a></li>
+                <li><a href="connecter.php">Se connecter</a></li>
+
+                
+            </ul>
+            </p>
+        </nav>
+            <section>
+            
+            <h1>Bienvenue au paradis des légumes et des fruit!!!</h1>
+            <p>
+                <article>
+                Il vous manque un ingrédient pour vos repas ? Vous avez besoin d'un légume satisfaisant votre fine <mark>exigence</mark> ?
+                Vous êtes à la bonne adresse. Sur ce site Internet vous pourrez échanger des légumes ou des fruits entre membres 
+                priviligiés. Vous decouvrerez des vendeurs, ou des particuliers, prochent de chez vous afin que vos envies culinaires 
+                soient rasasiées. Vous pourrez en temps que membre publiez les photographies et atout de vos propres légume afin de  
+                les échanger avec notre communauté.
+                <article/>
+            
+            </p>
+            <p>
+                Vous pouvez rechercher un fruit ou d’un légume, par catégories ou variété mais aussi par saison, par origine,
+                par maturité, par poids, par quantité. Inscrivez vous et partager, recevez des produit les plus savoureux les 
+                uns que les autres 
+            </p>
+            
+
+            </section>
+        </div>
+    </body>
+    </html>
