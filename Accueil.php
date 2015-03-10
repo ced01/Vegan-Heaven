0aaab994323a6fd3ae0fc1520af8ced69caf2a64
ced01 <cedlautard@gmail.fr>
2015-03-10 15:23:12 +0100

    Modification multiculturelle

--- Accueil.php
+++ Accueil.php
@@ -34,7 +34,7 @@
                 Vous êtes à la bonne adresse. Sur ce site Internet vous pourrez échanger des légumes ou des fruits entre membres 
                 priviligiés. Vous decouvrerez des vendeurs, ou des particuliers, prochent de chez vous afin que vos envies culinaires 
                 soient rasasiées. Vous pourrez en temps que membre publiez les photographies et atout de vos propres légume afin de  
-                les échanger avec notre communauté.
+                les échanger avec notre communauté multiculturelle.
                 <article/>
             
             </p>
