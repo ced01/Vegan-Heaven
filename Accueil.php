5a66a4481f7120ffbb6e5771efc3d8398b4e4985
ced01 <lautard1@hotmail.fr>
2015-03-10 17:09:56 +0100

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -16,7 +16,7 @@
             <p>
             <ul>
                 
-                <li><a href="Accueil.html">Accueil</a><img src="bloggif_54ddf13e82321.jpeg" class="imageflottante"  alt="Logo VeganHeaven"/></li>
+                <li><a href="Accueil.php">Accueil</a><img src="bloggif_54ddf13e82321.jpeg" class="imageflottante"  alt="Logo VeganHeaven"/></li>
                 <li><a href="Recherche.php">Voir les offres</a></li>
                 <li><a href="FAQ.php">A propos</a></li>
                 <li><a href="Forum.php">A propos</a></li>
