48cbc6ba0fe664f77665cc685f41c0fca6e5d684
ced01 <cedlautard@gmail.fr>
2015-03-10 16:53:02 +0100

    Ajout de la page css et modification de la page d'accueil

--- Accueil.php
+++ Accueil.php
@@ -3,19 +3,20 @@
     <head>
            <meta charset="utf-8" />
             <title>VeganHeaven</title>
+            <link rel="stylesheet" href="StyleGraphique.css" />
             <!--[if lt IE 9]>
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
             <![endif]-->
     </head>
      <body>
         <div>
-
+             <img src="bloggif_54ddf2c4e18c5.jpeg"  alt="Logo VeganHeaven" />
 
         <nav>
             <p>
             <ul>
                 
-                <li><a href="Accueil.php">Accueil</a></li>
+                <li><img src="bloggif_54ddf13e82321.jpeg" class="imageflottante"  alt="Logo VeganHeaven"/><a href="Accueil.html">Accueil</a></li>
                 <li><a href="Recherche.php">Voir les offres</a></li>
                 <li><a href="FAQ.php">A propos</a></li>
                 <li><a href="Forum.php">A propos</a></li>
@@ -28,20 +29,13 @@
             <section>
             
             <h1>Bienvenue au paradis des légumes et des fruit!!!</h1>
-            <p>
+            <p class="dessous" class="1">
                 <article>
                 Il vous manque un ingrédient pour vos repas ? Vous avez besoin d'un légume satisfaisant votre fine <mark>exigence</mark> ?
                 Vous êtes à la bonne adresse. Sur ce site Internet vous pourrez échanger des légumes ou des fruits entre membres 
                 priviligiés. Vous decouvrerez des vendeurs, ou des particuliers, prochent de chez vous afin que vos envies culinaires 
                 soient rasasiées. Vous pourrez en temps que membre publiez les photographies et atout de vos propres légume afin de  
                 les échanger avec notre communauté multiculturelle.
-                <article/>
-            
-            </p>
-            <p>
-                Vous pouvez rechercher un fruit ou d’un légume, par catégories ou variété mais aussi par saison, par origine,
-                par maturité, par poids, par quantité. Inscrivez vous et partager, recevez des produit les plus savoureux les 
-                uns que les autres. 
             </p>
             
 
